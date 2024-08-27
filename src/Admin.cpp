627ef41de8632ffe01e25250d9f5b9836b0bebfa
Quang6574 <nightfury5a3@gmail.com>
2024-08-27 17:25:04 +0700

    Update Admin.cpp
    view info(?)

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -1,5 +1,6 @@
 #include "Admin.h"
 #include <iostream>
+#include <ifstream>
 
 using namespace std;
 
@@ -11,7 +12,35 @@ void Admin::viewAdminInfo() const {
   cout << "Admin password: " << adminPassword << endl;
 }
 
-void Admin::viewUserInfo() const {
+void Admin::viewUserInfo(string username) const {
+    
+    ifstream readStream;
+    
+    string usernameData;
+    string fullname;
+    int phonenum;
+    string idtype;
+    int id;
+    int credit;
+    
+    readStream.open("userInfo.dat", std::ios::in);
+    if (!readStream) cout << "Fail to access user info data."
+    ;
+
+    while (getline(readStream, usernameData, ' ') {
+        if (username == usernameData) {
+            
+            readStream.ignore(18, ' ');
+            getline(readStream, fullname, '/');
+
+            readStream >> phonenum >> idtype >> id >> credit;
+
+            cout << "Name: " << fullname << '\n';
+            cout << "Phone number: " << phonenum << '\n';
+            cout << idtype << " ID: " << id << '\n';
+            cout << "Credit: " << credit << '\n';
+        }
+        
     
 }
 
